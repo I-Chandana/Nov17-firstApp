af5e6736437cfd7ce7e59a5c6da94cf7c136aed9
I-Chandana <88822109+I-Chandana@users.noreply.github.com>
2021-11-19 12:09:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
+import { injected } from "./components/wallet/connectors" 
+import { useWeb3React } from "@web3-react/core";
 
 function App() {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React()
 
   const [messages, setMessages] = useState();
 
@@ -13,9 +16,29 @@ function App() {
       })
   }, []);
 
+  async function connect() {
+    try {
+      console.log("click")
+      await activate(injected)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate()
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
   return (
     <div>
       <p>{messages}</p>
+      <button onClick={connect}> Connect to MeataMask</button>
+      {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+      <button onClick={disconnect}></button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Web3ReactProvider } from '@web3-react/core';
+import Web3 from 'web3';
+
+function getLibrary(provider){
+  return new Web3(provider)
+}
 
 ReactDOM.render(
+  <Web3ReactProvider getLibrary={getLibrary}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Web3ReactProvider>,
   document.getElementById('root')
 );
 
